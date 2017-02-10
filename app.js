0dd0de7ef06806bdc0ecd5b3875ef80547d76894
Philip Rosen <philipjrosen@gmail.com>
2017-02-10 15:47:30 -0800

    Replace in memory with redis

--- app.js
+++ app.js
@@ -6,20 +6,32 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var cities = {
-  'Lotopia': 'description',
-  'Caspiana': 'description',
-  'Indigo': 'description'
-};
-
+// Redis connection
+var redis = require('redis');
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
+}
+// End Redis Connection
 app.get('/cities', function(req, res) {
-  res.json(Object.keys(cities));
+   client.hkeys('cities', function(error, names) {
+    if(error) throw error;
+
+    res.json(names);
+  });
 });
 
 app.post('/cities', urlencode, function(req, res) {
   var newCity = req.body;
-  cities[newCity.name] = newCity.description;
-  res.status(201).json(newCity.name);
+  client.hset('cities', newCity.name, newCity.description, function(error) {
+    if(error) throw error;
+
+    res.status(201).json(newCity.name);
+  });
 });
 
 module.exports = app;
